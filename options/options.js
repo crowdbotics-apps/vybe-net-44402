648c0001548a77329cd9543f83f4947161ffeb96
Crowdbotics <team@crowdbotics.com>
2023-11-07 21:55:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vybe-net-44402",
+  url: "https://vybe-net-44402.botics.co",
+  api: "https://vybe-net-44402.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
